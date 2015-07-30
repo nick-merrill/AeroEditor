c5a8922cfca6e94ee2d8fc7ee37dfd0c6cb688ee
Nick Merrill <git@nickmerrill.co>
2015-07-29 23:19:59 -0600

    Initial Commit

---
+++ AeroEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AeroEditor
+//
+//  Created by Nick Merrill on 7/29/15.
+//  Copyright (c) 2015 Nick Merrill. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AeroEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AeroEditor
+//
+//  Created by Nick Merrill on 7/29/15.
+//  Copyright (c) 2015 Nick Merrill. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
